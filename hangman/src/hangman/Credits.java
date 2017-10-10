9ad1c22b5507fff660cfa101f978403b8e5261bc
Oscar alcaraz <oalcaraz@cpp.edu>
2017-10-10 10:56:01 -0700

    Added the Project Info to each Class

--- hangman/src/hangman/Credits.java
+++ hangman/src/hangman/Credits.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/***************************************************************
+ * 
+ * file: Credits.java
+ * author: Team Ji-Jo
+ * class: CS 245 - Programming Graphical User Interfaces
+ *
+ * assignment: Hangman V1.0
+ * date last modified: 10/10/17
+ *
+ * purpose: This class is the credits page that will show the names and ID numbers of the team members.  
+ *
+ ****************************************************************/
+
 package hangman;
 
 /**
