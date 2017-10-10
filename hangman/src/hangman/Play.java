f2559601d97e05e3428c7d6b13949c6b4eb3b2a9
Oscar alcaraz <oalcaraz@cpp.edu>
2017-10-10 10:55:31 -0700

    Added the Project Info to each Class

--- hangman/src/hangman/Play.java
+++ hangman/src/hangman/Play.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/***************************************************************
+ * 
+ * file: Play.java
+ * author: Team Ji-Jo
+ * class: CS 245 - Programming Graphical User Interfaces
+ *
+ * assignment: Hangman V1.0
+ * date last modified: 10/10/17
+ *
+ * purpose: This class is the actual Hangman Game.
+ *
+ ****************************************************************/
+
 package hangman;
 
 import java.util.*;
