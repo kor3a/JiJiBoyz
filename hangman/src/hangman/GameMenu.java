0a79ea08d15b6bcded6238edde77c0341f7cabe7
ieabbas <the0nlyismail@gmail.com>
2017-10-30 23:49:56 -0700

    Added reference code to exit game from menu screen using escape button, need to implement fully

--- hangman/src/hangman/GameMenu.java
+++ hangman/src/hangman/GameMenu.java
@@ -95,4 +95,15 @@ public class GameMenu extends JPanel implements ActionListener {
             }
         }
     }
+    
+    public final void escapeListener() {
+         getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+         KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel");
+         getRootPane().getActionMap().put("Cancel", new AbstractAction() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+            }
+         });
+     }
 }
