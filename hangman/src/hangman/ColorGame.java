76c6d49fac61bf8afa7a2d63d693caddf3e94255
Subong Jeon <subongjeon@cpp.edu>
2017-10-31 13:44:32 -0700

    Changes made in colorgame class so it goes to sudoku after it ends

--- hangman/src/hangman/ColorGame.java
+++ hangman/src/hangman/ColorGame.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package hangman;
+
 import java.awt.*;
 import static java.lang.Thread.sleep;
 import java.util.*;
@@ -11,12 +12,13 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.lang.reflect.Field;
 import javax.swing.*;
+
 /**
  *
  * @author josue
  */
 public class ColorGame extends JPanel implements ActionListener {
-    
+
     private Random rng = new Random();
     private JLabel clockDisplay;
     private JLabel scoreDisplay;
@@ -29,11 +31,11 @@ public class ColorGame extends JPanel implements ActionListener {
     private boolean h_ball3;
     private boolean h_ball4;
     private boolean h_ball5;
-    private  Ellipse2D ball1;
-    private  Ellipse2D ball2;
-    private  Ellipse2D ball3;
-    private  Ellipse2D ball4;
-    private  Ellipse2D ball5;
+    private Ellipse2D ball1;
+    private Ellipse2D ball2;
+    private Ellipse2D ball3;
+    private Ellipse2D ball4;
+    private Ellipse2D ball5;
     private Graphics2D title;
     private Graphics2D g_ball1;
     private Graphics2D g_ball2;
@@ -41,116 +43,109 @@ public class ColorGame extends JPanel implements ActionListener {
     private Graphics2D g_ball4;
     private Graphics2D g_ball5;
     private Graphics2D highlight;
-    
-    
+
     private final String[] COLORS = {"GREEN", "RED", "BLUE",
         "YELLOW", "MAGENTA"
     };
     private String currentColor;
     private String currentColorTitle;
-    
-    public ColorGame(GameIntro g, int score){
-      this.g = g;
-      this.score = score;
-      randomizeColor();
-      loadGUI();
-      clock();
-      counter = 5;
-      
+
+    public ColorGame(GameIntro g, int score) {
+        this.g = g;
+        this.score = score;
+        randomizeColor();
+        loadGUI();
+        clock();
+        counter = 5;
+
     }
 
     private void loadGUI() {
         setLayout(null);
         clockDisplay = new JLabel("");
-        scoreDisplay = new JLabel("Score: "+score);
+        scoreDisplay = new JLabel("Score: " + score);
         skip = new JButton("Skip");
         skip.addActionListener(this);
         skip.setBounds(490, 40, 80, 30);
         clockDisplay.setBounds(200, 1, 250, 25);
         scoreDisplay.setBounds(120, 1, 200, 25);
-        
+
         add(clockDisplay);
         add(scoreDisplay);
         add(skip);
         repaint();
-       
-        addMouseMotionListener(new MouseAdapter(){
+
+        addMouseMotionListener(new MouseAdapter() {
             @Override
-            public void mouseMoved(MouseEvent e){
+            public void mouseMoved(MouseEvent e) {
                 h_ball1 = ball1.contains(e.getPoint());
                 h_ball2 = ball2.contains(e.getPoint());
                 h_ball3 = ball3.contains(e.getPoint());
                 h_ball4 = ball4.contains(e.getPoint());
                 h_ball5 = ball5.contains(e.getPoint());
                 repaint();
-                
+
             }
         });
-         addMouseListener(new MouseAdapter(){
-             @Override
-            public void mouseClicked(MouseEvent e){
-                
-       if((e.getButton()== 1)&&counter == 0){
-           exit();
-        }
-        if((e.getButton() == 1)&& (ball1.contains(e.getX(), e.getY())) && g_ball1.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1)&& (ball2.contains(e.getX(), e.getY())) && g_ball2.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1)&& (ball2.contains(e.getX(), e.getY())) && g_ball2.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1)&& (ball3.contains(e.getX(), e.getY())) && g_ball3.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1)&& (ball4.contains(e.getX(), e.getY())) && g_ball4.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1)&& (ball5.contains(e.getX(), e.getY())) && g_ball5.getColor() == title.getColor() ){
-            randomizeColor();
-            counter--;
-            score += 100;
-            scoreDisplay.setText("Score: " + Integer.toString(score));
-        }
-        else if((e.getButton() == 1) && ((ball1.contains(e.getX(), e.getY())) || (ball2.contains(e.getX(), e.getY())) || (ball3.contains(e.getX(), e.getY()))
-                || (ball4.contains(e.getX(), e.getY())) || (ball5.contains(e.getX(), e.getY())) )){
-            randomizeColor();
-            counter--;
-            
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                if ((e.getButton() == 1) && counter == 0) {
+                    exit();
+                }
+                if ((e.getButton() == 1) && (ball1.contains(e.getX(), e.getY())) && g_ball1.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && (ball2.contains(e.getX(), e.getY())) && g_ball2.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && (ball2.contains(e.getX(), e.getY())) && g_ball2.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && (ball3.contains(e.getX(), e.getY())) && g_ball3.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && (ball4.contains(e.getX(), e.getY())) && g_ball4.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && (ball5.contains(e.getX(), e.getY())) && g_ball5.getColor() == title.getColor()) {
+                    randomizeColor();
+                    counter--;
+                    score += 100;
+                    scoreDisplay.setText("Score: " + Integer.toString(score));
+                } else if ((e.getButton() == 1) && ((ball1.contains(e.getX(), e.getY())) || (ball2.contains(e.getX(), e.getY())) || (ball3.contains(e.getX(), e.getY()))
+                        || (ball4.contains(e.getX(), e.getY())) || (ball5.contains(e.getX(), e.getY())))) {
+                    randomizeColor();
+                    counter--;
+
+                }
+
             }
-         
-     }
-       
-            
-         });   
-        
+
+        });
+
     }
-    public void exit(){
+
+    public void exit() {
         g.frame.getContentPane().setVisible(false);
         g.frame.getContentPane().remove(this);
-        g.frame.add(new HighScores(g, score));
+        g.frame.add(new Sudoku(g, score));
         g.frame.getContentPane().setVisible(true);
-            
+
     }
-   
-     public void clock() {
+
+    public void clock() {
         Thread clock = new Thread() {
             public void run() {
                 try {
@@ -174,14 +169,10 @@ public class ColorGame extends JPanel implements ActionListener {
         };
         clock.start();
     }
-    
-     
-    
-     
-     
-   @Override
+
+    @Override
     public void actionPerformed(ActionEvent e) {
-       Object object = e.getSource();
+        Object object = e.getSource();
         JButton button = null;
 
         if (object instanceof JButton) {
@@ -199,18 +190,18 @@ public class ColorGame extends JPanel implements ActionListener {
                 g.frame.getContentPane().setVisible(true);
             }
         }
-    
     }
+
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        title = (Graphics2D)g;
-        Font titleFont = new Font("Arial Black", Font.BOLD, 20); 
+        title = (Graphics2D) g;
+        Font titleFont = new Font("Arial Black", Font.BOLD, 20);
         title.setFont(titleFont);
-        title.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        title.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         title.setColor(getColorValue(currentColor));
         title.drawString(currentColorTitle, 280, 50);
-        
+
         //ball 1
         g_ball1 = (Graphics2D) g;
         ball1 = new Ellipse2D.Double(15, 100, 100, 100);
@@ -231,53 +222,47 @@ public class ColorGame extends JPanel implements ActionListener {
         g_ball5 = (Graphics2D) g;
         ball5 = new Ellipse2D.Double(450, 180, 100, 100);
         ballStats(ball5, g_ball5, "MAGENTA");
-       
+
         highlight = (Graphics2D) g;
         highlight.setColor(Color.BLACK);
-        if(h_ball1){
-            highlight.drawOval(15, 100, 102, 102); 
+        if (h_ball1) {
+            highlight.drawOval(15, 100, 102, 102);
+        } else if (h_ball2) {
+            highlight.drawOval(100, 200, 102, 102);;
+        } else if (h_ball3) {
+            highlight.drawOval(250, 200, 102, 102);;
+        } else if (h_ball4) {
+            highlight.drawOval(350, 100, 102, 102);;
+        } else if (h_ball5) {
+            highlight.drawOval(450, 180, 102, 102);;
         }
-        else if(h_ball2){
-            highlight.drawOval(100, 200, 102, 102); ;
-        }
-        else if(h_ball3){
-            highlight.drawOval(250, 200, 102, 102); ;
-        }
-        else if(h_ball4){
-            highlight.drawOval(350, 100, 102, 102); ;
-        }
-        else if(h_ball5){
-            highlight.drawOval(450, 180, 102, 102); ;
-        }
-        
-        
+
     }
-   private void ballStats (Ellipse2D shape, Graphics2D ball, String color){
-       ball.setColor(getColorValue(color));
-       ball.fill(shape);
-        
-   }
-   private Color getColorValue(String color) {
-        try{
-           final Field f = Color.class.getField(color);
-           return (Color)f.get(null);
-       }catch(Exception e){
-           return Color.BLACK;
-       }
+
+    private void ballStats(Ellipse2D shape, Graphics2D ball, String color) {
+        ball.setColor(getColorValue(color));
+        ball.fill(shape);
+
     }
-   
-   //makes sure they don't have the same color
+
+    private Color getColorValue(String color) {
+        try {
+            final Field f = Color.class.getField(color);
+            return (Color) f.get(null);
+        } catch (Exception e) {
+            return Color.BLACK;
+        }
+    }
+
+    //makes sure they don't have the same color
     private void randomizeColor() {
-      int first_num = rng.nextInt(5);
-      int next_num = rng.nextInt(5);
-      currentColor = COLORS[first_num];
-      while(first_num == next_num){
-          next_num = rng.nextInt(5);
-      }
-      currentColorTitle = COLORS[next_num];
+        int first_num = rng.nextInt(5);
+        int next_num = rng.nextInt(5);
+        currentColor = COLORS[first_num];
+        while (first_num == next_num) {
+            next_num = rng.nextInt(5);
+        }
+        currentColorTitle = COLORS[next_num];
     }
-   
-   
-  
-    
+
 }
